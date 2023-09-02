e71b30002915926d2b2331875174e5dbffdffb5b
Alex Hadley <alexhad6@users.noreply.github.com>
2023-09-01 17:50:11 -0700

    Finish working customized setup

---
+++ datalogger_jupyterlab.py
@@ -0,0 +1,8 @@
+# Appears to be needed only for running "jupyter labextension develop --overwrite ."
+# in development. Thus, this module is not included in the distribution.
+
+def _jupyter_labextension_paths():
+    return [{
+        "src": "labextension",
+        "dest": "datalogger_jupyterlab"
+    }]
