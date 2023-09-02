817cea0999587299a8a1c56c614828b0494bad66
Alex Hadley <alexhad6@users.noreply.github.com>
2023-09-02 01:14:44 -0700

    Create initial context menu item

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,47 @@
 import { JupyterFrontEnd, JupyterFrontEndPlugin } from "@jupyterlab/application";
+import { IFileBrowserFactory } from "@jupyterlab/filebrowser";
+import { showDialog, Dialog } from "@jupyterlab/apputils";
+import { spreadsheetIcon, codeIcon } from "@jupyterlab/ui-components";
 
-/**
- * Initialization data for the jupyterlab_apod extension.
- */
+// TODO: These can be separate plugins within the same extension
+
 const plugin: JupyterFrontEndPlugin<void> = {
-  id: "datalogger_jupyterlab:plugin",
+  id: "datalogger-jupyterlab:plugin",
   description: "JupyterLab plugin for DataLogger",
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    console.log("JupyterLab extension datalogger_jupyterlab is activated!");
+  requires: [IFileBrowserFactory],
+  activate: (app: JupyterFrontEnd, factory: IFileBrowserFactory) => {
+    // Add NetCDF file type
+    const docRegistry = app.docRegistry;
+    if (docRegistry.getFileType("netcdf") === undefined) {
+      docRegistry.addFileType({
+        name: "netcdf",
+        displayName: "NetCDF File",
+        extensions: [".nc"],
+        mimeTypes: ["application/x-netcdf"],
+        contentType: "file",
+        fileFormat: "base64",
+        icon: spreadsheetIcon,
+      });
+    }
+
+    // Context menu command
+    app.commands.addCommand("datalogger-jupyterlab:open", {
+      label: "Copy DataLogger Load Code",
+      caption: "Example context menu button for file browser's items.",
+      icon: codeIcon,
+      execute: () => {
+        const file = factory.tracker.currentWidget?.selectedItems().next().value;
+
+        if (file) {
+          showDialog({
+            title: file.name,
+            body: `load_log("${file.path}")`,
+            buttons: [Dialog.okButton()],
+          }).catch((e) => console.log(e));
+        }
+      },
+    });
   },
 };
 
