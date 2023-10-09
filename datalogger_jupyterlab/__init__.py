19a0c4f49ce9e7eb2420d56c44fe13e9c9d46180
Alex Hadley <alexhad6@users.noreply.github.com>
2023-10-09 12:21:33 -0700

    Fix dev mode and add pack package.json description and homepage

--- datalogger_jupyterlab/__init__.py
+++ datalogger_jupyterlab/__init__.py
@@ -1,8 +1,4 @@
-# Appears to be needed only for running "jupyter labextension develop --overwrite ."
-# in development. Thus, this module is not included in the distribution.
+"""JupyterLab extension configuration."""
 
 def _jupyter_labextension_paths():
-    return [{
-        "src": "labextension",
-        "dest": "datalogger-jupyterlab"
-    }]
+    return [{"src": "../labextension", "dest": "datalogger-jupyterlab"}]
