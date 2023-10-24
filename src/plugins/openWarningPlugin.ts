434cbdebf662ce1d7f56538d15eab67bc033883f
Alex Hadley <alexhad6@users.noreply.github.com>
2023-10-24 13:34:53 -0700

    #5 Add comments to open warning plugin code

--- src/plugins/openWarningPlugin.ts
+++ src/plugins/openWarningPlugin.ts
@@ -6,15 +6,20 @@ import { User } from "@jupyterlab/services";
 import { IGlobalAwareness } from "@jupyter/collaboration";
 import { showDialog, Dialog } from "@jupyterlab/apputils";
 
+/** State within the awareness object. */
 type AwarenessState = {
   user?: User.IIdentity;
   current?: string | null;
   timestamp?: number;
 };
 
+/**
+ * Plugin that displays a warning dialog when opening a file that another user has open.
+ */
 const openWarningPlugin: JupyterFrontEndPlugin<void> = {
   id: "datalogger-jupyterlab:open-warning",
-  description: "Displays a warning when opening a file that another user has open.",
+  description:
+    "Displays a warning dialog when opening a file that another user has open.",
   autoStart: true,
   requires: [IDocumentManager, IGlobalAwareness],
   activate: (
@@ -22,22 +27,42 @@ const openWarningPlugin: JupyterFrontEndPlugin<void> = {
     docManager: IDocumentManager,
     awareness: Awareness,
   ) => {
+    /**
+     * Saved string corresponding to the current document, or null if not on a document.
+     */
     let savedCurrent: string | null = null;
+
+    /**
+     * Saved timestamp for when the current document was opened. If another user has the
+     * file open, this will be compared against their timestamp to determine whether to
+     * display a warning dialog. If null, then no warning dialog will be displayed.
+     */
     let savedTimestamp: number | null = null;
+
+    /**
+     * Whether there is currently a warning dialog open, to prevent additional dialogs
+     * from being opened simultaneously.
+     */
     let dialogIsOpen = false;
 
     awareness.on("change", async () => {
-      console.log(new Map(awareness.getStates()));
-      console.log(`Timestamp is ${savedTimestamp}`);
-
       const states: Map<number, AwarenessState> = awareness.getStates();
       const myClientID = awareness.clientID;
       const myCurrent = states.get(myClientID)?.current ?? null;
+
+      // If the current document is different than the saved document, update the saved
+      // document and timestamp, and update the timestamp in the awareness; otherwise,
+      // display a warning dialog if another user has the same document open with an older
+      // timestamp
       if (myCurrent !== savedCurrent) {
         savedCurrent = myCurrent;
         savedTimestamp = Date.now();
         awareness.setLocalStateField("timestamp", savedTimestamp);
       } else if (!dialogIsOpen && savedCurrent !== null && savedTimestamp !== null) {
+        // Find the oldest state among other users who are on the same document and have
+        // an older timestamp. (The purpose of finding the user with the *oldest*
+        // timestamp, not just *an* older timestamp, is to display the name of the user
+        // with the highest priority in the dialog warning message.)
         let oldestState: {
           clientID: number;
           user?: User.IIdentity;
@@ -59,11 +84,17 @@ const openWarningPlugin: JupyterFrontEndPlugin<void> = {
           }
         }
 
+        // If another user on the same document has an older timestamp, display a dialog
+        // warning message
         if (oldestState !== null) {
           const { clientID, user } = oldestState;
           const pathComponents = savedCurrent.split(":");
           const filename = path.basename(pathComponents[2]);
+
+          // Prevent other dialogs from being opened simultaneously
           dialogIsOpen = true;
+
+          // Display the warning dialog
           const {
             button: { accept },
           } = await showDialog({
@@ -76,10 +107,17 @@ const openWarningPlugin: JupyterFrontEndPlugin<void> = {
               Dialog.okButton({ label: "Close" }),
             ],
           });
+
+          // Setting the saved timestamp to null prevents additional dialogs for being
+          // shown until the user switches away and back to this file
           savedTimestamp = null;
+
+          // If "Close" was selected, attempt to close the file
           if (accept) {
             await docManager.closeFile(pathComponents.slice(1).join(":"));
           }
+
+          // Allow future dialogs to be opened
           dialogIsOpen = false;
         }
       }
